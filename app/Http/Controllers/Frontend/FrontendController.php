396415f4eddae406c058e1cd7a044eeee8317393
rojina <rojinat@ekbana.com>
2017-12-08 13:30:25 +0545

    working on frontend module

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -9,38 +9,17 @@ use App\Homecomponent;
 // use App\Event;
 use App\GalleryImage;
 use App\Page;
+use App\Setting;
 
 class FrontendController extends Controller
 {
 
     public function index()
     {
-        $bannerData=Banner::all();
-        $homeComponent=Homecomponent::all();
-        $aboutus=Page::where([['page_title','About'],['slug','/about-us']])->first();
-        $pageData=Page::where([['page_title','Home'],['slug','/home']])->first();
-        return view('frontend.home',compact('bannerData','homeComponent','aboutus','pageData'));
+      
+      $webSetting=Setting::first();
+      
+  return view('frontend.home',compact('webSetting'));
     }
-    public function about()
-    {
-       $images=GalleryImage::where('gallery_id',1)->where('status','Active')->get();
-        $pageData=Page::where([['page_title','About'],['slug','/about-us']])->first();
-        return view('frontend.aboutus',compact('pageData','images'));
-    }
-    public function contact()
-    {
-        return view('frontend.contact');
-    }
-
-    public function gallery()
-    {
-        $pageData=Page::where([['page_title','Gallery'],['slug','/gallery']])->first();
-        return view('frontend.gallery',compact('pageData'));
-    }
-
-    public function events()
-    {
-     $pageData=Page::where([['page_title','Events'],['slug','/events']])->first();
-     return view('frontend.events',compact($pageData));
- }
+   
 }
--- routes/frontend.php
+++ routes/frontend.php
@@ -1,2 +1,8 @@
 <?php
-Route::get('about' ,'Frontend\FrontendController@about');
+
+/*frontend routes */
+Route::get('/','Frontend\FrontendController@index');
+Route::get('/aboutus','Frontend\FrontendController@about');
+Route::get('/our-event','Frontend\FrontendController@events');
+Route::get('/contact-us','Frontend\FrontendController@contact');
+Route::get('/gallery','Frontend\FrontendController@gallery');
