3fa019c72fbc44808b26457b4802a281c34a28d2
rojina <rojinat@ekbana.com>
2018-05-10 14:37:54 +0545

    gallery section added in homepage frontend

---
+++ resources/views/frontend/includes/gallery.blade.php
@@ -0,0 +1,356 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- bootstrap css -->
+  <link href="{{URL::asset('frontend/gallery/css/bootstrap.min.css')}}" rel="stylesheet">
+  <!-- css -->
+  <link href="{{URL::asset('frontend/gallery/css/style.css')}}" rel="stylesheet">
+  <link href="{{URL::asset('frontend/gallery/css/animate.css')}}" rel="stylesheet">
+  <link href="{{URL::asset('frontend/gallery/css/magnific-popup.css')}}" rel="stylesheet">
+  <!-- fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,700" rel="stylesheet">
+  <link href="{{URL::asset('frontend/gallery/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="{{URL::asset('frontend/gallery/css/linear-icons.css')}}">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+</head>
+
+<body class="gallery-page">
+
+  <!-- preloader -->
+  <div id="preloader">
+    <div class="spinner spinner-round"></div>
+  </div>
+  <!-- / preloader -->
+
+
+
+  <!-- content -->
+  <div id="page-content" class="container-fluid">
+    <!-- projects -->
+    <section id="portfolio" class="no-gutter">
+      <ul class="row portfolio list-unstyled gallery" id="grid">
+
+        <!-- project -->
+        <li class="col-md-4 col-sm-3 project" data-groups='["portrait"]'>
+          <figure class="portfolio-item">
+            <div class="hovereffect">
+              <img class="img-responsive" src="{{URL::asset('frontend/gallery/images/project1.jpg')}}" alt="">
+              <a href="{{URL::asset('frontend/gallery/images/project1.jpg')}}">
+                <div class="overlay">
+                  <div class="hovereffect-title">
+                    <h5>Photo 1</h5>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </figure>
+        </li>
+        <!-- / project -->
+
+        <!-- project -->
+        <li class="col-md-4 col-sm-3 project portrait-img" data-groups='["wedding"]'>
+          <figure class="portfolio-item">
+            <div class="hovereffect">
+              <img class="img-responsive" src="{{URL::asset('frontend/gallery/images/project2.jpg')}}" alt="">
+              <a href="{{URL::asset('frontend/gallery/images/project2.jpg')}}">
+                <div class="overlay">
+                  <div class="hovereffect-title">
+                    <h5>Photo 2</h5>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </figure>
+        </li>
+        <!-- / project -->
+
+        <!-- project -->
+        <li class="col-md-4 col-sm-3 project" data-groups='["wedding"]'>
+          <figure class="portfolio-item">
+            <div class="hovereffect">
+              <img class="img-responsive" src="{{URL::asset('frontend/gallery/images/project3.jpg')}}" alt="">
+              <a href="{{URL::asset('frontend/gallery/images/project3.jpg')}}">
+                <div class="overlay">
+                  <div class="hovereffect-title">
+                    <h5>Photo 3</h5>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </figure>
+        </li>
+        <!-- / project -->
+
+        <!-- project -->
+        <li class="col-md-4 col-sm-3 project" data-groups='["architecture", "landscape", "photography"]'>
+          <figure class="portfolio-item">
+            <div class="hovereffect">
+              <img class="img-responsive" src="{{URL::asset('frontend/gallery/images/project4.jpg')}}" alt="">
+              <a href="{{URL::asset('frontend/gallery/images/project4.jpg')}}">
+                <div class="overlay">
+                  <div class="hovereffect-title">
+                    <h5>Photo 4</h5>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </figure>
+        </li>
+        <!-- / project -->
+
+        <!-- project -->
+        <li class="col-md-4 col-sm-3 project" data-groups='["architecture", "landscape", "photography"]'>
+          <figure class="portfolio-item">
+            <div class="hovereffect">
+              <img class="img-responsive" src="{{URL::asset('frontend/gallery/images/project5.jpg')}}" alt="">
+              <a href="{{URL::asset('frontend/gallery/images/project5.jpg')}}">
+                <div class="overlay">
+                  <div class="hovereffect-title">
+                    <h5>Photo 5</h5>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </figure>
+        </li>
+        <!-- / project -->
+
+        <!-- project -->
+        <li class="col-md-4 col-sm-3 project portrait-img" data-groups='["portrait"]'>
+          <figure class="portfolio-item">
+            <div class="hovereffect">
+              <img class="img-responsive" src="{{URL::asset('frontend/gallery/images/project6.jpg')}}" alt="">
+              <a href="{{URL::asset('frontend/gallery/images/project6.jpg')}}">
+                <div class="overlay">
+                  <div class="hovereffect-title">
+                    <h5>Photo 6</h5>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </figure>
+        </li>
+        <!-- / project -->
+
+        <!-- project -->
+        <li class="col-md-4 col-sm-3 project" data-groups='["portrait", "wedding"]'>
+          <figure class="portfolio-item">
+            <div class="hovereffect">
+              <img class="img-responsive" src="{{URL::asset('frontend/gallery/images/project7.jpg')}}" alt="">
+              <a href="{{URL::asset('frontend/gallery/images/project7.jpg')}}">
+                <div class="overlay">
+                  <div class="hovereffect-title">
+                    <h5>Photo 7</h5>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </figure>
+        </li>
+        <!-- / project -->
+
+        <!-- project -->
+        <li class="col-md-4 col-sm-3 project portrait-img" data-groups='["wedding", "photography"]'>
+          <figure class="portfolio-item">
+            <div class="hovereffect">
+              <img class="img-responsive" src="{{URL::asset('frontend/gallery/images/project8.jpg')}}" alt="">
+              <a href="{{URL::asset('frontend/gallery/images/project8.jpg')}}">
+                <div class="overlay">
+                  <div class="hovereffect-title">
+                    <h5>Photo 8</h5>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </figure>
+        </li>
+        <!-- / project -->
+
+        <!-- project -->
+        <li class="col-md-4 col-sm-3 project" data-groups='["portrait"]'>
+          <figure class="portfolio-item">
+            <div class="hovereffect">
+              <img class="img-responsive" src="{{URL::asset('frontend/gallery/images/project9.jpg')}}" alt="">
+              <a href="{{URL::asset('frontend/gallery/images/project9.jpg')}}">
+                <div class="overlay">
+                  <div class="hovereffect-title">
+                    <h5>Photo 9</h5>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </figure>
+        </li>
+        <!-- / project -->
+
+        <!-- project -->
+        <li class="col-md-4 col-sm-3 project" data-groups='["portrait", "landscape"]'>
+          <figure class="portfolio-item">
+            <div class="hovereffect">
+              <img class="img-responsive" src="{{URL::asset('frontend/gallery/images/project10.jpg')}}" alt="">
+              <a href="{{URL::asset('frontend/gallery/images/project10.jpg')}}">
+                <div class="overlay">
+                  <div class="hovereffect-title">
+                    <h5>Photo 10</h5>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </figure>
+        </li>
+        <!-- / project -->
+
+        <!-- project -->
+        <li class="col-md-4 col-sm-3 project portrait-img" data-groups='["portrait", "photography"]'>
+          <figure class="portfolio-item">
+            <div class="hovereffect">
+              <img class="img-responsive" src="{{URL::asset('frontend/gallery/images/project11.jpg')}}" alt="">
+              <a href="{{URL::asset('frontend/gallery/images/project11.jpg')}}">
+                <div class="overlay">
+                  <div class="hovereffect-title">
+                    <h5>Photo 11</h5>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </figure>
+        </li>
+        <!-- / project -->
+
+        <!-- project -->
+        <li class="col-md-4 col-sm-3 project" data-groups='["photography"]'>
+          <figure class="portfolio-item">
+            <div class="hovereffect">
+              <img class="img-responsive" src="{{URL::asset('frontend/gallery/images/project12.jpg')}}" alt="">
+              <a href="{{URL::asset('frontend/gallery/images/project12.jpg')}}">
+                <div class="overlay">
+                  <div class="hovereffect-title">
+                    <h5>Photo 12</h5>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </figure>
+        </li>
+        <!-- / project -->
+
+        <!-- project -->
+        <li class="col-md-4 col-sm-3 project" data-groups='["portrait"]'>
+          <figure class="portfolio-item">
+            <div class="hovereffect">
+              <img class="img-responsive" src="{{URL::asset('frontend/gallery/images/project13.jpg')}}" alt="">
+              <a href="{{URL::asset('frontend/gallery/images/project13.jpg')}}">
+                <div class="overlay">
+                  <div class="hovereffect-title">
+                    <h5>Photo 13</h5>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </figure>
+        </li>
+        <!-- / project -->
+
+        <!-- project -->
+        <li class="col-md-4 col-sm-3 project" data-groups='["photography"]'>
+          <figure class="portfolio-item">
+            <div class="hovereffect">
+              <img class="img-responsive" src="{{URL::asset('frontend/gallery/images/project14.jpg')}}" alt="">
+              <a href="{{URL::asset('frontend/gallery/images/project14.jpg')}}">
+                <div class="overlay">
+                  <div class="hovereffect-title">
+                    <h5>Photo 14</h5>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </figure>
+        </li>
+        <!-- / project -->
+
+        <!-- project -->
+        <li class="col-md-4 col-sm-3 project" data-groups='["architecture"]'>
+          <figure class="portfolio-item">
+            <div class="hovereffect">
+              <img class="img-responsive" src="{{URL::asset('frontend/gallery/images/project15.jpg')}}" alt="">
+              <a href="{{URL::asset('frontend/gallery/images/project15.jpg')}}">
+                <div class="overlay">
+                  <div class="hovereffect-title">
+                    <h5>Photo 15</h5>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </figure>
+        </li>
+        <!-- / project -->
+
+        <!-- project -->
+        <li class="col-md-4 col-sm-3 project" data-groups='["landscape"]'>
+          <figure class="portfolio-item">
+            <div class="hovereffect">
+              <img class="img-responsive" src="{{URL::asset('frontend/gallery/images/project16.jpg')}}" alt="">
+              <a href="{{URL::asset('frontend/gallery/images/project16.jpg')}}">
+                <div class="overlay">
+                  <div class="hovereffect-title">
+                    <h5>Photo 16</h5>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </figure>
+        </li>
+        <!-- / project -->
+
+        <!-- sizer -->
+        <li class="col-md-4 col-sm-3 shuffle_sizer"></li>      
+      </ul> <!-- / projects -->
+    </section>
+    <!-- / portfolio section 4col -->
+  </div><!-- / page-content -->
+  <!-- / content -->
+
+  <!-- / footer -->
+
+  <!-- javascript -->
+  <script src="{{URL::asset('frontend/gallery/js/jquery.min.js')}}"></script>
+  <script src="{{URL::asset('frontend/gallery/js/bootstrap.min.js')}}"></script>
+  <script src="{{URL::asset('frontend/gallery/js/jquery.easing.min.js')}}"></script>
+
+  <!-- preloader -->
+  <script src="{{URL::asset('frontend/gallery/js/preloader.js')}}"></script>
+  <!-- / preloader -->
+
+  <!-- portfolio script -->
+  <script src="{{URL::asset('frontend/gallery/js/jquery.shuffle.min.js')}}"></script>
+  <script src="{{URL::asset('frontend/gallery/js/custom.js')}}"></script>
+  <!-- / portfolio script -->
+
+  <!-- lightbox -->
+  <script src="{{URL::asset('frontend/gallery/js/jquery.magnific-popup.min.js')}}"></script>
+  <script type="text/javascript">
+// This will create a single gallery from all elements that have class "gallery-item"
+$('.gallery').each(function() { // the containers for all your galleries
+  $(this).magnificPopup({
+        delegate: 'a', // the selector for gallery item
+        type: 'image',
+        gallery: {
+          enabled:true
+        }
+      });
+});
+</script>
+<!-- / lightbox -->
+<!-- / javascript -->
+</body>
+
+</html>
